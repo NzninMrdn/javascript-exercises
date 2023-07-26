23a48a1dd175c3702aabd4e4d9aab021c3bbd452
Nazanin <nazanin16mrdn@gmail.com>
2023-07-26 15:15:13 +0330

    leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(num) {
 
+    let isLeap = false;
+
+    if(num % 4 === 0 && (num % 100 !== 0 || num % 400 === 0)){
+        isLeap =  true;
+    }
+
+    return isLeap;
 };
 
 // Do not edit below this line
