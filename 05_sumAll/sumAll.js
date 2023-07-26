6d003bfb7ebf92b99670c4dc983a6f099f0e84a4
Nazanin <nazanin16mrdn@gmail.com>
2023-07-26 15:03:35 +0330

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
 
+    if(!Number.isInteger(first) || !Number.isInteger(last)){
+        return "ERROR";
+    }
+
+    if (first < 0 || last < 0) return "ERROR";
+
+    let sum = first + last;
+
+    if(first < last){
+        for(let i = first+1 ; i<last; i++){
+            sum += i;
+        }
+    } else{
+        for(let i = last+1 ; i<first; i++){
+            sum += i;
+        } 
+    }
+
+
+    return sum;
+    
 };
 
 // Do not edit below this line
